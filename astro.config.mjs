fda5dde6a15e1b8473df280bce7cbceb36ee7574
darrinz <darrinz@umich.edu>
2023-08-01 22:49:56 -0500

    init-final

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://test.michigandaily.com",
+  base: "/2023/travel-edition/",
+});
